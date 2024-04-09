7895f6b2b79fca1818d95648a57026d9184c2de3
adityathakkar178 <160093854+adityathakkar178@users.noreply.github.com>
2024-04-09 16:40:00 +0530

    contract-update

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.12 <0.9.0;
 
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
 contract Example is EIP712 {
@@ -12,39 +12,32 @@ contract Example is EIP712 {
         address verifyingContract;
     }
 
-    bytes32 private constant EIP712_DOMAIN_TYPEHASH =
-        keccak256(
-            "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
-        );
     bytes32 private constant MESSAGE_TYPEHASH =
-        keccak256("Message(string message)");
+        keccak256("Message(string message, uint256 value, address sender)");
 
-    bytes32 private DOMAIN_SEPARATOR;
-
-    constructor() EIP712("Ether Mail", "1") {
-        DOMAIN_SEPARATOR = keccak256(
-            abi.encode(
-                EIP712_DOMAIN_TYPEHASH,
-                keccak256("Ether Mail"),
-                keccak256("1"),
-                11155111,
-                address(this)
-            )
-        );
-    }
+    constructor() EIP712("Ether Mail", "1") {}
 
     function verifySignature(
         string memory message,
+        uint256 value,
+        address senderAddress,
         bytes memory signature,
         address signerAddress
     ) public view returns (bool) {
         bytes32 messageHash = keccak256(
-            abi.encode(MESSAGE_TYPEHASH, keccak256(bytes(message)))
+            abi.encode(
+                MESSAGE_TYPEHASH,
+                keccak256(bytes(message)),
+                value,
+                senderAddress
+            )
         );
-        bytes32 structHash = keccak256(
-            abi.encodePacked("\x19\x01", DOMAIN_SEPARATOR, messageHash)
-        );
-        address recoveredSigner = ECDSA.recover(structHash, signature);
-        return recoveredSigner == signerAddress;
+        bytes32 structHash = _hashTypedDataV4(messageHash);
+        return
+            SignatureChecker.isValidSignatureNow(
+                signerAddress,
+                structHash,
+                signature
+            );
     }
 }
